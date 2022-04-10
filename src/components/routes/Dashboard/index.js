6f1f4949022b610ac1ba7287526e39860ab9087f
Thuan Tang <thuantang@gmail.com>
2022-04-09 22:18:45 -0700

    Updated <Dashboard /> for application intro and volume warning

--- src/components/routes/Dashboard/index.js
+++ src/components/routes/Dashboard/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { SafeAreaView, ScrollView, Text, View } from 'react-native';
 import { NAVBAR } from '../../../constants/tailwindcss';
 import FocusAwareStatusBar from '../../FocusAwareStatusBar';
+import tw from 'twrnc';
 
 const Dashboard = () => {
   return (
@@ -12,8 +13,32 @@ const Dashboard = () => {
         barStyle='light-content'
         backgroundColor={NAVBAR.backgroundColor}
       />
-      <ScrollView>
-        <Text>Dashboard</Text>
+      <ScrollView style={tw`m-2`}>
+        <Text style={tw`font-bold text-lg text-center mb-5`}>
+          Welcome to Tinnitus Relief - A Sound Therapy Application
+        </Text>
+        <Text style={tw`font-bold text-lg text-center text-pink-600`}>
+          PLEASE LOWER YOUR VOLUME
+        </Text>
+        <Text style={tw`font-bold text-lg text-center mb-5 text-pink-600`}>
+          BEFORE USING THIS APP TO PROTECT YOUR EARS
+        </Text>
+        <Text style={tw`mb-2`}>Sound Therapy consists of:</Text>
+        <Text style={tw`mb-2`}>
+          - ACRN Tonal - Use the tone sound generator to test your tinnitus
+          frequency
+        </Text>
+        <Text style={tw`mb-2`}>
+          - ACRN Sequence - The sequence generator shoud be played at your
+          tinnitus frequency
+        </Text>
+        <Text style={tw`mb-2`}>
+          - Sounds - Play sounds uploaded by other users for masking your
+          tinnitus
+        </Text>
+        <Text style={tw`mb-2`}>
+          - Mixes - Create a combination of sounds for an immersive ambient
+        </Text>
       </ScrollView>
     </SafeAreaView>
   );
